e59d63f7dda6b1a399b54ebc1d2cad4eed2087eb
AlexPi <alex.pigida@outlook.com>
2022-11-01 10:34:45 -0400

    Update UpdateGitHubRepo.bat

--- Src/UpdateGitHubRepo.bat
+++ Src/UpdateGitHubRepo.bat
@@ -1,4 +1,24 @@
+@echo Off
 
 set para=/FFT /R:1 /W:1 /XD .git .gitignore .vs .vscode node_modules dist bin obj /XF *.deploy *.user *.suo *.manifest *.pdb *.vshost.exe* EntityFramework*.xml *.application /NJH /NJS /NDL 
+
+@echo.
+@echo.
+@echo.
+@echo    robocopy    C:\Users\alexp\source\repos\alex-pi    C:\g\alex-pi\Src   ...
+@echo.
+@echo.
     
 robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%       /MIR 
+@echo.
+
+set para=
+
+@echo.
+@echo.
+@echo      ******************* The End ***********************
+@echo      ******************* The End ***********************
+@echo      ******************* The End ***********************
+@echo      ******************* The End ***********************
+
+timeout 26
