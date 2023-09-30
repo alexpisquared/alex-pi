5646c71e7b45d843d293acd3c2af7ea33fee5381
alexpisqueared <alex.pigida@outlook.com>
2023-09-30 09:01:25 -0400

    pau

--- Src/Db.OneBase/Model.Ext/OneBaseContext.cs
+++ Src/Db.OneBase/Model.Ext/OneBaseContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -8,11 +7,17 @@ namespace Db.OneBase.Model;
 
 public partial class OneBaseContext //: DbContext
 {
-  public OneBaseContext() { }
-  //public OneBaseContext(DbContextOptions<OneBaseContext> options) { }
+  public OneBaseContext() { } // public OneBaseContext(DbContextOptions<OneBaseContext> options) { }
   public virtual DbSet<GuestbookMsg> GuestbookMsg { get; set; }
-  public virtual IQueryable<VwUserHopsUtc> VwUserHopsUtc => new VwUserHopsUtc[] { new() { Id = 1, Nickname = "Aaa", Finished = DateTime.Now }, new() { Id = 2, Nickname = "Bbb", Finished = DateTime.Now } }.AsQueryable();
-  public virtual DbSet<VwEventUserUtc> VwEventUserUtc { get; set; }
+  public virtual IQueryable<VwUserHopsUtc> VwUserHopsUtc => new VwUserHopsUtc[] {
+    new() {      Id = 1, Nickname = "Aaa", Hops=11, TotalMin=111, Started=DateTime.Today, Finished = DateTime.Now },
+    new() {      Id = 2, Nickname = "Bbb", Hops=22, TotalMin=222, Started=DateTime.Today, Finished = DateTime.Now },
+    new() {      Id = 3, Nickname = "Bbb", Hops=33, TotalMin=333, Started=DateTime.Today, Finished = DateTime.Now } }.AsQueryable();
+  public virtual IQueryable<VwEventUserUtc> VwEventUserUtc => new VwEventUserUtc[] {
+    new() {      Nickname = "Aaa", EventName="Aaa", DoneAt= DateTime.Now },
+    new() {      Nickname = "Bbb", EventName="Bbb", DoneAt= DateTime.Now },
+    new() {      Nickname = "Bbb", EventName="Bbb", DoneAt= DateTime.Now } }.AsQueryable();
+
   public virtual DbSet<WebEventLog> WebEventLog { get; set; }
   public virtual DbSet<WebsiteUser> WebsiteUser { get; set; }
 
