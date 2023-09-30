9ced6ae0c10cf3023881d658f519012b775ebf5a
alexpisqueared <alex.pigida@outlook.com>
2023-09-30 16:06:26 -0400

    Update OneBaseContext.cs

--- Src/Db.OneBase/Model.Ext/OneBaseContext.cs
+++ Src/Db.OneBase/Model.Ext/OneBaseContext.cs
@@ -10,13 +10,13 @@ public partial class OneBaseContext //: DbContext
   public OneBaseContext() { } // public OneBaseContext(DbContextOptions<OneBaseContext> options) { }
   public virtual DbSet<GuestbookMsg> GuestbookMsg { get; set; }
   public virtual IQueryable<VwUserHopsUtc> VwUserHopsUtc => new VwUserHopsUtc[] {
-    new() {      Id = 1, Nickname = "Aaa", Hops=11, TotalMin=111, Started=DateTime.Today, Finished = DateTime.Now },
-    new() {      Id = 2, Nickname = "Bbb", Hops=22, TotalMin=222, Started=DateTime.Today, Finished = DateTime.Now },
-    new() {      Id = 3, Nickname = "Bbb", Hops=33, TotalMin=333, Started=DateTime.Today, Finished = DateTime.Now } }.AsQueryable();
+    new() {      Id = 1, Nickname = "AaaNick", Hops=11, TotalMin=111, Started=DateTime.Today, Finished = new DateTime(2020,1,1) },
+    new() {      Id = 2, Nickname = "BbbNick", Hops=22, TotalMin=222, Started=DateTime.Today, Finished = new DateTime(2020,2,2) },
+    new() {      Id = 3, Nickname = "CccNick", Hops=33, TotalMin=333, Started=DateTime.Today, Finished = DateTime.Now } }.AsQueryable();
   public virtual IQueryable<VwEventUserUtc> VwEventUserUtc => new VwEventUserUtc[] {
-    new() {      Nickname = "Aaa", EventName="Aaa", DoneAt= DateTime.Now },
-    new() {      Nickname = "Bbb", EventName="Bbb", DoneAt= DateTime.Now },
-    new() {      Nickname = "Bbb", EventName="Bbb", DoneAt= DateTime.Now } }.AsQueryable();
+    new() {      Nickname = "AaaNick", EventName="Aaa Event Name", DoneAt= new DateTime(2020,1,1)},
+    new() {      Nickname = "BbbNick", EventName="Bbb Event Name", DoneAt= new DateTime(2020,2,2)},
+    new() {      Nickname = "BbbNick", EventName="Bbb Event Name", DoneAt= DateTime.Now } }.AsQueryable();
 
   public virtual DbSet<WebEventLog> WebEventLog { get; set; }
   public virtual DbSet<WebsiteUser> WebsiteUser { get; set; }
