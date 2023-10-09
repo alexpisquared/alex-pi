96c8e9816d1d5a6ef77f48e1df194cc70a763124
alexpisqueared <alex.pigida@outlook.com>
2023-10-09 14:11:30 -0400

    Update WebEventLog.cs

--- Src/Db.OneBase/Model.Ext/WebEventLog.cs
+++ Src/Db.OneBase/Model.Ext/WebEventLog.cs
@@ -2,6 +2,8 @@
 
 namespace Db.OneBase.Model
 {
-  public partial record WebEventLog {[NotMapped] public string BrowserSignature { get; set; } }
+  public partial record WebEventLog {[NotMapped] public string BrowserSignature { get; set; }
+    public string ToString2 => $"---{BrowserSignature} ";
+  }
   public partial class WebsiteUser {[NotMapped] public int? VisitCount__New { get; set; } } // new / unreviewed visits  
 }
