9ac5e976866c34dcc8a90a7143e8aa66f5080084
alexpisqueared <alex.pigida@outlook.com>
2024-01-13 14:15:50 -0500

    ++

--- Src/UpdateGitHubRepo.bat
+++ Src/UpdateGitHubRepo.bat
@@ -1,6 +1,7 @@
 @echo Off
 
 set para=/FFT /R:1 /W:1 /XD .git .gitignore .vs .vscode node_modules dist bin obj /XF *.deploy *.user *.suo *.manifest *.pdb *.vshost.exe* EntityFramework*.xml *.application /NJH /NJS /NDL 
+set say=%OneDrive%\Public\bin\say.exe
 
 @echo.
 @echo.
@@ -10,28 +11,53 @@ set para=/FFT /R:1 /W:1 /XD .git .gitignore .vs .vscode node_modules dist bin ob
 @echo.
     
 @REM robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%  /MIR   /XF UserMap.json    :DELETES!!! if new file is in the wrong place: source
-robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%       /S /XO /XF UserMap.json
+robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%       /S /XO /XF UserMap.json  /L
 
 :menu
+start /min %say% "hm?"
 @echo.
+echo [99;92m   Redo    MIR    Quit [0m 
+echo [99;92m   Redo    MIR    Quit [0m 
+echo [99;92m   Redo    MIR    Quit [0m 
 @echo.
-choice /C 12 /N /M "1. MIR / Delete!!!    2. Exit? "
-if errorlevel 2 goto exit
-if errorlevel 1 goto open
+echo [99;93m   Auto quitting in 9 sec [0m 
+@echo.
+choice /T 9 /C 1rm4q /N /D q
+echo [99;94m   selected  %ERRORLEVEL% [0m 
+start /min %say% selected  %ERRORLEVEL%
+call :%ERRORLEVEL%
+start /min %say% "Review for 2 seconds"
+timeout /T 2 >Nul
 goto menu
 
-:open
-robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%  /MIR   /XF UserMap.json    
+:10
+:9
+:8
+:7
+:6
+start /min %say% %ERRORLEVEL% - selected 
+:5
 goto end
-
-:exit
-goto end
+:4
+goto :EOF
+:3
+  robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%  /MIR   /XF UserMap.json    
+goto :EOF
+:2
+:1
+  robocopy C:\Users\alexp\source\repos\alex-pi C:\g\alex-pi\Src %para%  /S /XO /XF UserMap.json
+goto :EOF
 
 :end
 @echo.
 @echo.
 @echo.
-@REM @echo      ******************* The End ***********************
+echo      [98;91m   ******************* The End *********************** [0m 
+echo      [97;92m   ******************* The End *********************** [0m 
+echo      [96;93m   ******************* The End *********************** [0m 
+echo      [96;94m   ******************* The End *********************** [0m 
+echo      [95;93m   ******************* The End *********************** [0m 
 set para=
-
-timeout 260
+set say=
+timeout %ERRORLEVEL%
+exit
